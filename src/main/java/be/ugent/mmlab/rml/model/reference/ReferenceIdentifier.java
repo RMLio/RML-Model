2e86be0c20e6297e63de087cb9f76a92cd1383cb
andimou <anastasia.dimou@ugent.be>
2015-06-17 11:36:58 +0200

    more files

---
+++ src/main/java/be/ugent/mmlab/rml/model/reference/ReferenceIdentifier.java
@@ -0,0 +1,19 @@
+package be.ugent.mmlab.rml.model.reference;
+
+/**
+ * RML - Model
+ * 
+ * This interface offers a method for replacing a template with the expression it holds
+ *
+ * @author mielvandersande
+ */
+public interface ReferenceIdentifier {
+    
+    /** Made a replaceAll on the input String to replace all occurrence of
+     * the "{parameter}" in.
+     * @param input The input String
+     * @return
+     */
+    public String replaceAll(String input, String replaceValue);
+    
+}
