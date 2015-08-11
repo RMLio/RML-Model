463ff380fe06a261c43e6ba11004b2a65213816a
andimou <anastasia.dimou@ugent.be>
2015-08-11 21:43:51 +0200

    improved SPARQL dsource

--- src/main/java/be/ugent/mmlab/rml/model/source/std/StdSparqlEndpointSource.java
+++ src/main/java/be/ugent/mmlab/rml/model/source/std/StdSparqlEndpointSource.java
@@ -9,8 +9,8 @@ import be.ugent.mmlab.rml.model.source.SparqlEndpointSource;
  * @author andimou
  */
 public class StdSparqlEndpointSource extends StdSource implements SparqlEndpointSource {
-    
-    //private String template;
+    private String supportedLanguage;
+    private String resultFormat;
     
     /**
      *
@@ -20,6 +20,30 @@ public class StdSparqlEndpointSource extends StdSource implements SparqlEndpoint
     public StdSparqlEndpointSource(String name, String template){
         super(name);
         setTemplate(template);
-    }   
+    } 
+    
+    public StdSparqlEndpointSource(String name, String template, 
+            String supportedLanguage, String resultFormat){
+        super(name);
+        setTemplate(template);
+        setSupportedLanguage(supportedLanguage);
+        setResultFormat(resultFormat);
+    } 
+    
+    private void setSupportedLanguage(String supportedLanguage){
+        this.supportedLanguage = supportedLanguage;
+    }
+    
+    public String getSupportedLanguage(){
+        return this.supportedLanguage;
+    }
+    
+    private void setResultFormat(String resultFormat){
+        this.resultFormat = resultFormat;
+    }
+    
+    public String getResultFormat(){
+        return this.resultFormat;
+    }
 
 }
--- src/main/java/be/ugent/mmlab/rml/vocabularies/SPARQLSDVocabulary.java
+++ src/main/java/be/ugent/mmlab/rml/vocabularies/SPARQLSDVocabulary.java
@@ -13,13 +13,24 @@ package be.ugent.mmlab.rml.vocabularies;
  */
 public class SPARQLSDVocabulary {
     public static String SPARQLSD_NAMESPACE = "http://www.w3.org/ns/sparql-service-description#";
+    public static String FORMATS_NAMESPACE = "http://www.w3.org/ns/formats/data/";
 
     public enum SparqlSdTerm implements Term {
 
         // SPARQL-SD CLASSES
         SERVICE_CLASS("Service"),
         // SPARQL-SD PROPERTIES
-        SPARQL_QUERY_TEMPLATE("sparqlQueryTemplate");
+        SPARQL_QUERY_TEMPLATE("sparqlQueryTemplate"),
+        SUPPORTEDLANGUAGE("supportedLanguage"),
+        RESULTFORMAT("resultFormat"),
+        //SPARQL-SD INSTANCES
+        SPARQL10QUERY("SPARQL10Query"), 
+        SPARQL11QUERY("SPARQL11Query"), 
+        SPARQL11UPDATE("SPARQL11Update"),
+        SPARQL_RESULTS_XML("SPARQL_Results_XML"),
+        SPARQL_RESULTS_JSON("SPARQL_Results_JSON"),
+        SPARQL_RESULTS_CSV("SPARQL_Results_CSV");
+        
         private String displayName;
 
         private SparqlSdTerm(String displayName) {
