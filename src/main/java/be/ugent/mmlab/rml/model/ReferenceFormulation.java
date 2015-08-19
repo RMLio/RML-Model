58daff2fb492b689a61c5ebdad909fc24aeb218c
andimou <anastasia.dimou@ugent.be>
2015-08-19 15:34:59 +0200

    added custom reference formulation

---
+++ src/main/java/be/ugent/mmlab/rml/model/ReferenceFormulation.java
@@ -0,0 +1,16 @@
+package be.ugent.mmlab.rml.model;
+
+/**
+ * *************************************************************************
+ *
+ * RML - Model : Reference Formulation
+ *
+ *
+ * @author andimou
+ *
+ ***************************************************************************
+ */
+public interface ReferenceFormulation {
+       
+    
+}
