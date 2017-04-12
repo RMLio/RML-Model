6b7ee976e2412788e2e0b970637fbb4d44795776
Serafini Alfredo <seralf@gmail.com>
2017-04-12 19:42:47 +0200

    updated CSVReferenceFormulation, for handling CSVW encoding cofngiration

--- src/main/java/be/ugent/mmlab/rml/model/std/CsvwReferenceFormulation.java
+++ src/main/java/be/ugent/mmlab/rml/model/std/CsvwReferenceFormulation.java
@@ -12,24 +12,36 @@ import org.slf4j.LoggerFactory;
  *
  ****************************************************************************/
 public class CsvwReferenceFormulation implements ReferenceFormulation {
-    private String delimiter ;
-    
-    // Log
-    private static final Logger log = 
-            LoggerFactory.getLogger(
-            CsvwReferenceFormulation.class.getSimpleName());
-    
-    public CsvwReferenceFormulation(){}
-    
-    public CsvwReferenceFormulation(String delimiter){
-        setDelimiter(delimiter);
-    }
-    
-    public String getDelimiter(){
-        return this.delimiter;
-    }
 
-    public final void setDelimiter(String delimiter){
-        this.delimiter = delimiter;
-    }
+	// Logger
+	private static final Logger logger = LoggerFactory.getLogger(CsvwReferenceFormulation.class.getSimpleName());
+
+	private String delimiter;
+	private String encoding;
+
+	public CsvwReferenceFormulation() {
+		this(",", "UTF-8"); // default values
+	}
+
+	public CsvwReferenceFormulation(final String delimiter, final String encoding) {
+		this.setDelimiter(delimiter);
+		this.setEncoding(encoding);
+	}
+
+	public String getDelimiter() {
+		return this.delimiter;
+	}
+
+	public final void setDelimiter(final String delimiter) {
+		this.delimiter = delimiter;
+	}
+
+	public String getEncoding() {
+		return this.delimiter;
+	}
+
+	public final void setEncoding(final String encoding) {
+		this.encoding = encoding;
+	}
+
 }
